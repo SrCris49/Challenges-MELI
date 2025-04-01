86145ebaf5addfbc895b9611a3043acb1db31ac8
Cristian Camilo Barragán <cbarragan2324@gmail.com>
2025-03-31 20:10:01 -0500

    cambio de credenciales

--- api-pokemon-iam/app.py
+++ api-pokemon-iam/app.py
@@ -11,7 +11,7 @@ blacklist = set()
 # Configuración básica
 load_dotenv()
 app = Flask(__name__)
-app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+app.config["ADMIN_PASSWORD"] = os.getenv("ADMIN_PASSWORD")
 jwt = JWTManager(app)
 
 # Configurar logging
